7406258926b653ef417fede35ce8295cc51b0f84
Reda EL Fillali <reda.elfillali@gmail.com>
2022-04-07 15:51:40 +0000

    create components and start mading the home page

---
+++ app/View/Components/Layout/Navbar.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\View\Components\Layout;
+
+use Illuminate\View\Component;
+
+class Navbar extends Component
+{
+  /**
+   * @var \string[][]
+   */
+  public array $navigationItems;
+
+
+  /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->navigationItems = [
+            ['label' => 'About', 'href' => '#about'],
+            ['label' => 'Projects', 'href' => '#projects'],
+            ['label' => 'Coding Tutorials', 'href' => '#tutorials'],
+            ['label' => 'Contact', 'href' => '#contact'],
+        ];
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('layout.navbar');
+    }
+}
