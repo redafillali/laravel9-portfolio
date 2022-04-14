ead1361956d7fd515391910500300d851fbc97eb
Reda EL Fillali <reda.elfillali@gmail.com>
2022-04-14 12:45:28 +0000

    front-end finalisation

--- resources/views/components/home/learn-php.blade.php
+++ resources/views/components/home/learn-php.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
-</div>
+<x-call-to-action>
+  <x-slot:title>
+    <h4 class="text-white text-base font-semibold mb-2">
+      Check my PHP Developer Roadmap
+    </h4>
+    <h2 class=" text-white font-bold text-3xl sm:text-[38px] leading-tight mb-6 sm:mb-8 lg:mb-0 ">
+      Start Learning <br class="hidden xs:block"/>
+      PHP For FREE
+    </h2>
+    </x-slot>
+    <x-button-link href="https://github.com/redafillali/laravel9-portfolio" variant="dark"
+                   class="mr-3 w-full mb-3 text-center lg:w-auto" target="_blank">
+      View on Github
+    </x-button-link>
+</x-call-to-action>
---
+++ resources/views/email/contact.blade.php
@@ -0,0 +1,4 @@
+<h2>New contact request</h2>
+<p><strong>Name : </strong> {{ $name }}</p>
+<p><strong>Email : </strong> {{ $email }}</p>
+<p><strong>Message : </strong> {{ $body }}</p>
